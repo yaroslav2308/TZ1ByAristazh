bc7959fa1ae49002f009bb34b88a901250b10c83
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-23 19:27:07 +0300

    Initial Commit

---
+++ TZ1ByAristazh/TZ1ByAristazhApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZ1ByAristazhApp.swift
+//  TZ1ByAristazh
+//
+//  Created by Yaroslav Monastyrev on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TZ1ByAristazhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
